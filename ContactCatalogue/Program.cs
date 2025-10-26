a45d1bb04fdb64dbd6165fba14e93a8dda8ad675
BwunLevain <vivienne114bengtsson@gmail.com>
2025-10-26 20:36:46 +0100

    Add project files.

---
+++ ContactCatalogue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactCatalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
